6620ad06736032d2fbb3ceceda00073dedefa175
Lior <liorcohen990@gmail.com>
2025-04-30 20:20:17 +0530

    #update

---
+++ projects/if else.py
@@ -0,0 +1,14 @@
+if 2 ** 3 == 8:
+    print("2*2*2=8")
+else:
+    print("Wrong")
+
+if 9 % 2 == 0:
+    print("9 % 2 = 0")
+else:
+    print("wrong")
+
+if 9 // 2 == 4:
+    print("yaay")
+else:
+    print("nope")
