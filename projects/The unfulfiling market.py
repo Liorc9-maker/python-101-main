3cb1da7946e3cbe9c740439c4bdf10e94f981cbe
Lior <liorcohen990@gmail.com>
2025-05-03 20:14:31 +0530

    3/5

---
+++ projects/The unfulfiling market.py
@@ -0,0 +1,7 @@
+print("Welcome to the unfulfilling market!")
+print("Tell us what you want, and we won't have it.")
+
+food = input("What do you want? ")
+amount = int(input(f"How much of {food}? "))
+
+print("You want {0amount} {food}? Sorry, we only have {amount -1}.")
