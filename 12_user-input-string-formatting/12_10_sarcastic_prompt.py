45ec7d7e151a8c1f8c6859efb419da64ece87a36
Lior <liorcohen990@gmail.com>
2025-05-05 19:55:38 +0530

    5/5

--- 12_user-input-string-formatting/12_10_sarcastic_prompt.py
+++ 12_user-input-string-formatting/12_10_sarcastic_prompt.py
@@ -1,3 +1,17 @@
 # Create a sarcastic program that asks a user for their honest opinion,
 # then prints the same sentence back to them in aLtErNaTiNg CaPs.
 sarcastic_text = input('Please share your honest opinion with me:\n')
+result = ''
+upper = True
+
+for char in sarcastic_text:
+    if char.isalpha():
+        if upper:
+            result += char.upper()
+        else:
+            result += char.lower()
+        upper = not upper
+    else:
+        result += char
+
+print(result)
