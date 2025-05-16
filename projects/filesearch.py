d17f2a8906e46ab8cfe0253910c492f02f7ff333
Lior <liorcohen990@gmail.com>
2025-05-16 20:09:04 +0530

    16/5

--- projects/filesearch.py
+++ projects/filesearch.py
@@ -14,3 +14,35 @@
 # Start with a small folder to make it easy to check whether your program is
 # working correctly. Then search a bigger folder.
 # This program should work for any specified folder on your computer.
+import pathlib
+
+base_path = pathlib.Path('/Users/liorc/Desktop/recycling amma')
+
+jpg_files = []
+all_extensions = set()
+
+for item in base_path.iterdir():
+    if item.is_file():
+        if item.suffix.lower() == ".jpg":
+            jpg_files.append(item.resolve())
+            all_extensions.add(item.suffix.lower())
+
+    elif item.is_dir():
+        for subitem in item.iterdir():
+            if subitem.is_file():
+                if subitem.suffix.lower() == ".jpg":
+                    jpg_files.append(subitem.resolve())
+                    all_extensions.add(subitem.suffix.lower())
+
+    elif subitem.is_dir():
+        for subsubitem in subitem.iterdir():
+            if subsubitem.suffix.lower() == ".jpg":
+                jpg_files.append(subsubitem.resolve())
+                all_extensions.add(subsubitem.suffix.lower())
+
+print("List of JPG files:")
+for jpg in jpg_files:
+    print(jpg)
+
+print("\nAll file extensions found:")
+print(all_extensions)          
--- projects/mover.py
+++ projects/mover.py
@@ -2,13 +2,18 @@
 # from one folder to another
 
 # Import pathlib
-
+import pathlib
 # Find the path to my Desktop
-
+desktop = pathlib.Path('/Users/liorc/Desktop')
 # Create a new folder
-
+new_path = pathlib.Path('/Users/liorc/Desktop/Screenshots')
+new_path.mkdir(exist_ok=True)
 # Filter for screenshots only
-
-# Create a new path for each file
-
+for file in desktop.iterdir():
+    if file.suffix.lower() ==  ".png":
+        print(file)
+        # Create a new path for each file
+        new_filepath = new_path.joinpath(file.name)
+        # Move the screenshot there
+        file.replace(new_filepath)
 # Move the screenshot there
